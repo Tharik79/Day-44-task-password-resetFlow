bf13d16ac52344bb06004235e4a3b5d2d966f00c
tharikuthuman <thaarik@gmail.com>
2022-01-14 01:53:38 +0530

    login form react UI completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Card, Form, Nav } from 'react-bootstrap';
+//import { Button, Form } from 'bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Login/>
     </div>
   );
 }
 
 export default App;
+
+
+function Login(){
+
+
+  return(
+
+    <div className='container'>
+
+      <Card bg='light' style={{ width: '500px', padding:'20px' }}>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+             We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Text id="passwordHelpBlock">
+            <Nav
+              activeKey="/home"
+                onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+              >
+                forget password ?
+                  <Nav.Item>
+                    <Nav.Link href="/home">reset here</Nav.Link>
+                  </Nav.Item>
+            </Nav>
+              
+               {/* Forget password ? <Link to="/signup" onClick={() => history.push('/signup')}>Sign Up</Link> */}
+          </Form.Text>
+        </Form.Group>  
+
+  
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+      </Form>
+      </Card>
+
+         
+
+    </div>
+
+  );
+};
